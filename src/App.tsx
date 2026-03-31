898ff1a8013522afa99cf80abf64c4668804a9b0
Pre Tchèssi <tchesspreza@yahoo.fr>
2026-03-31 12:44:43 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+
+export default function App() {
+
+  return (
+    <main style={{ fontFamily: 'system-ui, -apple-system, Segoe UI, Roboto, sans-serif', padding: 24 }}>
+      <h1>Vite + React + Hono ⚡</h1>
+    </main>
+  )
+}
